2028ff59e0c30fd3029d1ec909fc92d32d15375d
Simon <64349881+simon-cg@users.noreply.github.com>
2021-07-19 13:51:43 +0100

    Updates

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,16 @@ import Head from 'next/head'
 import '../styles/main.css'
 
 export default function Nextra({ Component, pageProps }) {
+  console.log("Nice to see you here")
   return (
     <>
       <Head>
-        <link
+        {/* <link
           rel="alternate"
           type="application/rss+xml"
           title="RSS"
           href="/feed.xml"
-        />
+        /> */}
         <link
           rel="preload"
           href="/fonts/Inter-roman.latin.var.woff2"
